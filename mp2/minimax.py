6f275702afbc6b0c4efa6c6cc7f9d644225f5e1d
the1mahn <msanghv2@illinois.edu>
2018-03-12 16:30:33 -0500

    Alpha-Beta Pruning in searchTree2

--- mp2/minimax.py
+++ mp2/minimax.py
@@ -1,10 +1,11 @@
 from Gomoku import *
 from copy import deepcopy
 
-def searchTree(board, depth):
+def searchTree(board, depth, counter):
+	counter += 1
 	if depth == 3:
 		weight = weighBoard(board)
-		return board, weight
+		return board, weight, counter
 	newboard = board
 	x = 8
 	y = 8
@@ -17,7 +18,8 @@ def searchTree(board, depth):
 			if (board.node_array[i][j].isBlue == False and board.node_array[i][j].isRed == False):
 				if depth == 1:
 					newboard.updateBlocks(i, j, 1)
-					updatedboard, weight = searchTree(newboard, depth + 1)
+					updatedboard, weight, newCounter = searchTree(newboard, depth + 1, counter)
+					counter = newCounter
 					newboard.updateBlocks(i, j, 0)
 					if (weight < value):
 						value = weight
@@ -25,7 +27,8 @@ def searchTree(board, depth):
 						y = j
 				else:
 					newboard.updateBlocks(i, j, 2)
-					updatedboard, weight = searchTree(newboard, depth + 1)
+					updatedboard, weight, newCounter = searchTree(newboard, depth + 1, counter)
+					counter = newCounter
 					newboard.updateBlocks(i, j, 0)
 					#print (depth, value, weight, x, y)
 					#updatedboard.printNodes()
@@ -38,7 +41,57 @@ def searchTree(board, depth):
 	#print(depth, x)
 	if depth == 0 and x != 8:
 		newboard.updateBlocks2(x, y, 2)
-	return newboard, value
+	return newboard, value, counter
+
+def searchTree2(board, depth, alpha, beta, counter):
+	counter += 1
+	if depth == 3:
+		weight = weighBoard(board)
+		return board, weight, counter
+	newboard = board
+	x = 8
+	y = 8
+	if depth == 1:
+		value = 1000
+	else:
+		value = -1000
+	for i in range(board.rows):
+		if (alpha > beta):
+			break
+		for j in range(board.cols):
+			if (alpha > beta):
+				break
+			if (board.node_array[i][j].isBlue == False and board.node_array[i][j].isRed == False):
+				if depth == 1:
+					newboard.updateBlocks(i, j, 1)
+					updatedboard, weight, newCounter = searchTree2(newboard, depth + 1, -2000, beta, counter)
+					counter = newCounter
+					newboard.updateBlocks(i, j, 0)
+					if (weight < beta):
+						beta = weight
+					if (weight < value):
+						value = weight
+						x = i
+						y = j
+				else:
+					newboard.updateBlocks(i, j, 2)
+					updatedboard, weight, newCounter = searchTree2(newboard, depth + 1, alpha, 2000, counter)
+					counter = newCounter
+					newboard.updateBlocks(i, j, 0)
+					#print (depth, value, weight, x, y)
+					#updatedboard.printNodes()
+					if (weight > alpha):
+						alpha = weight
+					if (weight > value):
+						value = weight
+						x = i
+						y = j
+						#print (depth, value, weight, x, y)
+
+	#print(depth, x)
+	if depth == 0 and x != 8:
+		newboard.updateBlocks2(x, y, 2)
+	return newboard, value, counter
 
 def weighBoard(board):
 	weight = 0
