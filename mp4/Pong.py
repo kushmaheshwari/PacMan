8f1c5314c9b6d98e4c8b1a15a4e2c2746347ac50
the1mahn <msanghv2@illinois.edu>
2018-04-27 19:22:32 -0500

    added paddle

--- mp4/Pong.py
+++ mp4/Pong.py
@@ -1,14 +1,18 @@
 import sys, pygame
+import time
 
 pygame.init()
 
 Gsize = Gwidth, Gheight = 600, 600
-Bsize = Bwidth, Bheight = 50, 50
+
 white = 250, 250, 250
 black = 0, 0, 0
 red = 255, 0, 0
+
+paddle_y = 240
 pos = [300, 300]
-velocity = [3, 1]
+rect = pygame.Rect(580, paddle_y, 20, 120)
+velocity = [18, 6]
 screen = pygame.display.set_mode(Gsize)
 
 while 1:
@@ -18,6 +22,9 @@ while 1:
     pos[0] = pos[0] + velocity[0]
     pos[1] = pos[1] + velocity[1]
 
+    time.sleep(.100)
+
     screen.fill(black)
-    pygame.draw.circle(screen, red, pos, 25, 0)
+    pygame.draw.circle(screen, red, pos, 10, 0)
+    pygame.draw.rect(screen, white, rect, 0)
     pygame.display.flip()
