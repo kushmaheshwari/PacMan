f1b965ee6ffa9f01eba6112d4ce23e26cead1c99
the1mahn <msanghv2@illinois.edu>
2018-05-01 00:51:42 -0500

    FML

--- mp4/Pong.py
+++ mp4/Pong.py
@@ -11,25 +11,111 @@ black = 0, 0, 0
 red = 255, 0, 0
 
 QL = QL()
-gamma = 0.8
 
 paddle_y = 240
 pos = [300, 300]
 rect = pygame.Rect(600, paddle_y, 20, 120)
-velocity = [18, 0]
+velocity = [18, 6]
 
-pygame.init()
-screen = pygame.display.set_mode(Gsize)
+#pygame.init()
+#screen = pygame.display.set_mode(Gsize)
 
-contGame = 100000
+contGame = 125000
+totalBounces = 0
 bounces = 0
 reward = 0
 
 while (contGame > 0):
+	#for event in pygame.event.get():
+	#	if event.type == pygame.QUIT: sys.exit()
+
+	#time.sleep(.02)
+
+	#print("yo")
+	#print (QL.QMatrix[11, 6, 1, 1, 1])
+	dVel = [velocity[0], velocity[1]]
+	if velocity[0] > 0:
+		dVel[0] = 1
+	else:
+		dVel[0] = -1
+	if velocity[1] > 9:
+		dVel[1] = 1
+	elif velocity[1] < -9:
+		dVel[1] = -1
+	else:
+		dVel[1] = 0
+
+	dPaddle = (paddle_y + 60)//50
+	possibleAction = QL.bestAction(pos[0]//50, pos[1]//50, dVel[0], dVel[1], dPaddle)
+	possibleAction -= 1
+	
+	#possibleAction = random.randrange(-1,2)
+	#print('Best Action')
+	#print(possibleAction)
+	if (possibleAction == -1 and paddle_y > 1):
+		paddle_y = paddle_y - 24
+	if (possibleAction == 1 and paddle_y < 479):
+		paddle_y = paddle_y + 24
+
+	pos[0] = pos[0] + velocity[0]
+	pos[1] = pos[1] + velocity[1]
+
+	if pos[1] < 0:
+		pos[1] = (-1 * pos[1])
+		velocity[1] = -1 * velocity[1]
+	elif pos[1] >= 600:
+		pos[1] = (1199 - pos[1])
+		velocity[1] = -1 * velocity[1]
+	if pos[0] < 0:
+		pos[0] = (-1 * pos[0])
+		velocity[0] = -1 * velocity[0]
+
+	if (pos[0] >= 600) and (pos[1] > paddle_y) and (pos[1] < (paddle_y+120)):
+		reward = 1
+		bounces += 1
+		totalBounces += 1
+		pos[0] = 1199 - pos[0]
+		u = (decimal.Decimal(random.randrange(-9, 9)))
+		v = (decimal.Decimal(random.randrange(-18, 18)))
+		velocity[0] = (-1*velocity[0]) + u
+		velocity[1] = velocity[1] + v
+		if (velocity[0] > -18):
+			velocity[0] = -18
+		QL.updateQMatrix(pos[0]//50, pos[1]//50, dVel[0], dVel[1], dPaddle, possibleAction, reward)
+	elif (pos[0] >= 600):
+		reward = -1
+		print('Bounces' + str(bounces))
+		print(contGame)
+		bounces = 0
+		contGame -= 1
+		if contGame == 10000:
+			totalBounces = 0
+		#print('Failed')
+		pos[0] = 599
+		QL.updateQMatrix(pos[0]//50, pos[1]//50, dVel[0], dVel[1], dPaddle, possibleAction, reward)
+		paddle_y = 240
+		pos = [300, 300]
+		velocity = [18, 6]
+	else:
+		reward = 0
+		QL.updateQMatrix(pos[0]//50, pos[1]//50, dVel[0], dVel[1], dPaddle, possibleAction, reward)
+
+	#screen.fill(black)
+	#pygame.draw.circle(screen, red, pos, 25, 0)
+	#rect = pygame.Rect(600, paddle_y, 20, 120)	
+	#pygame.draw.rect(screen, white, rect, 0)
+	#pygame.display.flip()
+	#print (contGame)
+
+print(totalBounces/10000)
+pygame.init()
+screen = pygame.display.set_mode(Gsize)
+
+while (True):
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT: sys.exit()
 
-	#time.sleep(.02)
+	time.sleep(.02)
 
 	#print("yo")
 	#print (QL.QMatrix[11, 6, 1, 1, 1])
@@ -92,7 +178,7 @@ while (contGame > 0):
 		QL.updateQMatrix(pos[0]//50, pos[1]//50, dVel[0], dVel[1], dPaddle, possibleAction, reward)
 		paddle_y = 240
 		pos = [300, 300]
-		velocity = [18, 0]
+		velocity = [18, 6]
 	else:
 		reward = 0
 		QL.updateQMatrix(pos[0]//50, pos[1]//50, dVel[0], dVel[1], dPaddle, possibleAction, reward)
@@ -103,4 +189,5 @@ while (contGame > 0):
 	pygame.draw.rect(screen, white, rect, 0)
 	pygame.display.flip()
 	#print (contGame)
+
 
--- mp4/QL.py
+++ mp4/QL.py
@@ -3,8 +3,8 @@ import random
 
 class QL:
 	def __init__(self):
-		self.gamma = 0.8
-		self.alpha = 0.2
+		self.gamma = 0.5
+		self.alpha = 0
 		self.QMatrix = np.zeros((12, 12, 2, 3, 12, 3))
 		self.NMatrix = np.zeros((12, 12, 2, 3, 12, 3))
 		self.GameOver = 0
@@ -16,6 +16,7 @@ class QL:
 		velocity = [velocity_x, velocity_y]
 		pos[0] = pos[0] + velocity[0]
 		pos[1] = pos[1] + velocity[1]
+		newPaddle_y = paddle_y + possibleAction
 
 		if pos[1] < 0:
 			pos[1] = (-1 * pos[1])
@@ -36,30 +37,31 @@ class QL:
 			velocity_x = 0
 		if velocity[0] < 0:
 			velocity[0] = 0
-	
+		
 		possibleAction += 1
 		#print('Print Values of Actions at current State')
 		#print(ball_x, ball_y, pos[0], pos[1], velocity_x, velocity_y, velocity[0], velocity[1], paddle_y, possibleAction)
 		#print(self.QMatrix[ball_x, ball_y, velocity_x, velocity_y, paddle_y])
 		#print(ball_x, ball_y, velocity_x, velocity_y, paddle_y)
-		if (pos[0] > 11) and (pos[1] != paddle_y):
-			#print('SuperFailed')
-			#self.GameOver -= 1
-			#print (reward)
-			self.QMatrix[ball_x][ball_y][velocity_x][velocity_y+1][paddle_y][possibleAction] += reward
-			self.NMatrix[ball_x][ball_y][velocity_x][velocity_y+1][paddle_y][possibleAction] += 1
 
-			#print (self.QMatrix[ball_x, ball_y, velocity_x, velocity_y+1, paddle_y, possibleAction])
-			#print (self.QMatrix[ball_x, ball_y, velocity_x, velocity_y+1, paddle_y])
+		self.alpha = .6/(.6 + self.NMatrix[ball_x][ball_y][velocity_x][velocity_y+1][paddle_y][possibleAction])
+
+		if (pos[0] > 11) and (pos[1] != paddle_y):
+			#print (reward)
+			change = self.QMatrix[ball_x][ball_y][velocity_x][velocity_y+1][paddle_y][possibleAction] 
+			new = change + self.alpha*(reward - self.gamma - change)
+			self.QMatrix[ball_x][ball_y][velocity_x][velocity_y+1][paddle_y][possibleAction] = new
+			if new != 0:
+				self.NMatrix[ball_x][ball_y][velocity_x][velocity_y+1][paddle_y][possibleAction] += 1
 		
-		elif(pos[0] < 12):# ball_x < 12):
-			#print(reward + max(self.QMatrix[pos[0], pos[1], velocity[0], velocity[1]+1, paddle_y]))
-			self.alpha = 50/(50 + self.NMatrix[ball_x][ball_y][velocity_x][velocity_y+1][paddle_y][possibleAction])
+		elif(pos[0] < 12):
 
 			change = self.QMatrix[ball_x][ball_y][velocity_x][velocity_y+1][paddle_y][possibleAction] 
-			self.QMatrix[ball_x][ball_y][velocity_x][velocity_y+1][paddle_y][possibleAction] = change + self.alpha*(reward + self.gamma*max(self.QMatrix[pos[0], pos[1], velocity[0], velocity[1]+1, paddle_y]) - change)
-			self.NMatrix[ball_x][ball_y][velocity_x][velocity_y+1][paddle_y][possibleAction] += 1
-			print(self.NMatrix[ball_x][ball_y][velocity_x][velocity_y+1][paddle_y])
+			new = change + self.alpha*(reward + self.gamma*max(self.QMatrix[pos[0]][pos[1]][velocity[0]][velocity[1]+1][newPaddle_y]) - change)
+			self.QMatrix[ball_x][ball_y][velocity_x][velocity_y+1][paddle_y][possibleAction] = new
+			if new != 0:
+				self.NMatrix[ball_x][ball_y][velocity_x][velocity_y+1][paddle_y][possibleAction] += 1
+			#print(self.NMatrix[ball_x][ball_y][velocity_x][velocity_y+1][paddle_y])
 
 	def bestAction(self, ball_x, ball_y, velocity_x, velocity_y, paddle_y):
 		ball_x = int(ball_x)
@@ -70,11 +72,14 @@ class QL:
 		#print(ball_x, ball_y, velocity_x, velocity_y, paddle_y)
 		#print (self.QMatrix[ball_x, ball_y, velocity_x, velocity_y+1, paddle_y]) 
 		randomInt = random.randrange(0, 10)
-		print('Threshold :' + str(20//(np.max(self.NMatrix[ball_x][ball_y][velocity_x][velocity_y+1][paddle_y])+1)))
-		if randomInt < 20//(np.max(self.NMatrix[ball_x][ball_y][velocity_x][velocity_y+1][paddle_y])+1):
-			return random.randrange(0, 3)
+		#print('Threshold :' + str(10000//(np.min(self.NMatrix[ball_x][ball_y][velocity_x][velocity_y+1][paddle_y])+1)))
+		if randomInt < 100//(np.max(self.NMatrix[ball_x][ball_y][velocity_x][velocity_y+1][paddle_y])+1):
+			rint = random.randrange(0, 3)
+			#print ('Random')
+			return rint
+
 		else:
-			print('Argmax')
-			print(self.QMatrix[ball_x][ball_y][velocity_x][velocity_y+1][paddle_y])
+			#print('Argmax')
+			#print(self.QMatrix[ball_x][ball_y][velocity_x][velocity_y+1][paddle_y])
 			return np.argmax(self.QMatrix[ball_x][ball_y][velocity_x][velocity_y+1][paddle_y]) 
 
